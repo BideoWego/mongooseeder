fb010bf6ce95d50619e955c8d4118a1444a3afce
Bideo Wego <bideowego@gmail.com>
2017-06-15 20:36:17 -0400

    Add more debug info

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const rcFilename = '.mongooseederrc';
 
 
 const globalize = (models) => {
+  console.info('globalize', Object.keys(mongoose.connection.collections));
   Object.keys(models).forEach((modelName) => {
     global[modelName] = mongoose.model(modelName);
   });
